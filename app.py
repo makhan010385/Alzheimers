04fbe07be3747fe29d5ce5e54e8068e61452943c
Makhan Kumbhkar <99782439+makhan010385@users.noreply.github.com>
2026-05-19 07:37:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -493,4 +493,4 @@ def compare_models():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+   app.run(host='0.0.0.0', debug=True)
